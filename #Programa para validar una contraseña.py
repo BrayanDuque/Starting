db6b81c16f7823e4d9877886ae04fe479d43a848
Brayan <65044665+BrayanDuque@users.noreply.github.com>
2020-05-26 17:17:48 -0500

    Add files via upload

---
+++ #Programa para validar una contraseña.py
@@ -0,0 +1,11 @@
+#Programa para validar una contraseña
+
+Clave = "narnia"
+Usuario = input("Ingresa una clave: ")
+while Usuario != Clave:
+    Usuario = input("Ingresa una clave: ")
+    print("La clave es correcta")
+    break
+    
+    
+  
